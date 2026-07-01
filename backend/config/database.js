e988e241db995da24750fc2b1ab55724b9307d32
nauval2704 <nauvaltg@gmail.com>
2026-07-01 22:43:57 +0700

    fix: memperbaiki database untuk prod

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,15 @@
 //Set up mongoose connection
 const mongoose = require("mongoose");
 // prod
-// const mongoDB = ""; 
+const mongoDB = "mongodb://rsbhayangkara:poldaaceh2021@36.66.36.106:29017/simrsba";
 // dev
-const mongoDB = "mongodb://127.0.0.1:29017/simrsbhy";
+// const mongoDB = "mongodb://rsbhayangkara:poldaaceh2021@103.76.174.237/simrsba";
+// local
+// const mongoDB = "mongodb://localhost:27017/simrsba";
 mongoose.connect(mongoDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true, 
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 mongoose.Promise = global.Promise;
